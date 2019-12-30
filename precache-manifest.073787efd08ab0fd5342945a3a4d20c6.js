254da96a0f0d93d5df9317dfc18d69b7f8c07250
Dimitris Selalmazidis <dimitris.selalmazidis@aexp.com>
2019-12-30 08:44:37 +0000

    Updates

---
+++ precache-manifest.073787efd08ab0fd5342945a3a4d20c6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7d2f2c3d04ed450e9e3f11d6295638cd",
+    "url": "/mywebsite/index.html"
+  },
+  {
+    "revision": "3adca2f89d3613f2fcbe",
+    "url": "/mywebsite/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "4249f91a1a9901531530",
+    "url": "/mywebsite/static/js/2.6fd2600c.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/mywebsite/static/js/2.6fd2600c.chunk.js.LICENSE"
+  },
+  {
+    "revision": "3adca2f89d3613f2fcbe",
+    "url": "/mywebsite/static/js/main.e63ea64e.chunk.js"
+  },
+  {
+    "revision": "4d310c41a6c6dd9ff1c5",
+    "url": "/mywebsite/static/js/runtime-main.9a0bc2b5.js"
+  }
+]);
