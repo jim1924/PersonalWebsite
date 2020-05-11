f319cce97b877e7e145eaeef953d33cf9f91c955
Dimitris Selalmazidis <dimitris.selalmazidis@aexp.com>
2020-05-11 09:17:43 +0100

    Updates

--- js/init.js
+++ js/init.js
@@ -124,46 +124,6 @@ jQuery(document).ready(function($) {
     animationSpeed: 600,
     randomize: false,
   });
-
-  /* ----------------------------------------------------*/
-  /*	contact form
-------------------------------------------------------*/
-
-  $('form#contactForm button.submit').click(function() {
-    $('#image-loader').fadeIn();
-
-    const contactName = $('#contactForm #contactName').val();
-    const contactEmail = $('#contactForm #contactEmail').val();
-    const contactSubject = $('#contactForm #contactSubject').val();
-    const contactMessage = $('#contactForm #contactMessage').val();
-
-    const data = 'contactName=' + contactName + '&contactEmail=' + contactEmail +
-               '&contactSubject=' + contactSubject + '&contactMessage=' + contactMessage;
-
-    $.ajax({
-
-	      type: 'POST',
-	      url: 'inc/sendEmail.php',
-	      data: data,
-	      success: function(msg) {
-        // Message was sent
-        if (msg == 'OK') {
-          $('#image-loader').fadeOut();
-          $('#message-warning').hide();
-          $('#contactForm').fadeOut();
-          $('#message-success').fadeIn();
-        }
-        // There was an error
-        else {
-          $('#image-loader').fadeOut();
-          $('#message-warning').html(msg);
-	            $('#message-warning').fadeIn();
-        }
-	      },
-
-    });
-    return false;
-  });
 });
 
 
