ebe449f72723889f4d52dcf35d502727c4e89a73
Dimitris Selalmazidis <dimitris.selalmazidis@aexp.com>
2020-06-06 21:09:05 +0100

    extracted description to json file

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -3,85 +3,74 @@ import PropTypes from 'prop-types';
 import Networks from './Networks';
 
 
-class Header extends Component {
-  render() {
-    const name = this.props.data.name;
-    const occupation = this.props.data.occupation;
-    const jobDescription = this.props.data.jobDescription;
-    const employer = this.props.data.employer;
-    const city = this.props.data.address.city;
-    const networks = <Networks social={this.props.data.social}/>;
+const Header =(props)=> {
+  const {name, occupation, jobDescription, headerStatement, employer, city}=props.data;
+  const networks = <Networks social={props.data.social}/>;
 
-    return (
-      <header id="home">
-        <nav id="nav-wrap">
-          <a className="mobile-btn" href="#nav-wrap" title="Show navigation">
+  return (
+    <header id="home">
+      <nav id="nav-wrap">
+        <a className="mobile-btn" href="#nav-wrap" title="Show navigation">
             Show navigation
-          </a>
-          <a className="mobile-btn" href="#home" title="Hide navigation">
+        </a>
+        <a className="mobile-btn" href="#home" title="Hide navigation">
             Hide navigation
-          </a>
+        </a>
 
-          <ul id="nav" className="nav">
-            <li className="current">
-              <a className="smoothscroll" href="#home">
+        <ul id="nav" className="nav">
+          <li className="current">
+            <a className="smoothscroll" href="#home">
                 Home
-              </a>
-            </li>
-            <li>
-              <a className="smoothscroll" href="#about">
+            </a>
+          </li>
+          <li>
+            <a className="smoothscroll" href="#about">
                 About
-              </a>
-            </li>
-            <li>
-              <a className="smoothscroll" href="#resume">
+            </a>
+          </li>
+          <li>
+            <a className="smoothscroll" href="#resume">
                 Resume
-              </a>
-            </li>
-            <li>
-              <a className="smoothscroll" href="#portfolio">
+            </a>
+          </li>
+          <li>
+            <a className="smoothscroll" href="#portfolio">
                 Works
-              </a>
-            </li>
-            <li>
-              <a className="smoothscroll" href="#testimonials">
+            </a>
+          </li>
+          <li>
+            <a className="smoothscroll" href="#testimonials">
                 Testimonials
-              </a>
-            </li>
-            <li>
-              <a className="smoothscroll" href="#contact">
+            </a>
+          </li>
+          <li>
+            <a className="smoothscroll" href="#contact">
                 Contact
-              </a>
-            </li>
-          </ul>
-        </nav>
+            </a>
+          </li>
+        </ul>
+      </nav>
 
-        <div className="row banner">
-          <div className="banner-text">
-            <h1 className="responsive-headline">I'm {name}.</h1>
-            <h3>
-              I'm a <span>{city}</span> based <span>{occupation}</span>, with a
-              variety of knowledge in different front and back end languages,
-              responsive frameworks, databases and best code practises. My
-              objective is to simply become the best engineer I can be
-              and to contribute to the industry of technology. I am currently
-              employed by
-              <span>{employer}</span> as a<span> {jobDescription}</span>.
-            </h3>
-            <hr />
-            <ul className="social">{networks}</ul>
-          </div>
+      <div className="row banner">
+        <div className="banner-text">
+          <h1 className="responsive-headline">I'm {name}.</h1>
+          <h3>
+              I'm a <span>{city}</span> based <span>{occupation}</span>{headerStatement}
+            <span>{employer}</span> as a<span> {jobDescription}</span>.
+          </h3>
+          <hr />
+          <ul className="social">{networks}</ul>
         </div>
+      </div>
 
-        <p className="scrolldown">
-          <a className="smoothscroll" href="#about">
-            <i className="icon-down-circle" />
-          </a>
-        </p>
-      </header>
-    );
-  }
-}
+      <p className="scrolldown">
+        <a className="smoothscroll" href="#about">
+          <i className="icon-down-circle" />
+        </a>
+      </p>
+    </header>
+  );
+};
 
 Header.propTypes={
   data: PropTypes.object.isRequired,
